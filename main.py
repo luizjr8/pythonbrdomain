9a600b8335be7b25cbc79e14a903188ce1d3c042
luizjr8 <luiz@fermen.to>
2016-11-09 18:05:28 -0400

    Included the data-handler class and file

--- main.py
+++ main.py
@@ -7,57 +7,48 @@ import json
 import requests
 from pathlib import Path
 from termcolor import colored
+from DomainBase import DomainsBase, States
 
 class DomainChecker:
-	avail = set();
-	taken = set();
-	locked = set();
-	delayed = set();
+	db = DomainsBase()
 
-	def __init__(self, fn):
-		print('Iniciando com o arquivo %s' % fn)
-		
-		# Checando se arquivo existe
-		if(not Path(fn).is_file()):
-			print(colored('Erro: Arquivo %s não existe' % fn,'red'))
-			sys.exit()
-		
-		# Abre o arquivo informado e faz o Regex
-		file = open(fn,'r').read()
-		r = re.findall('((?:[\w-]+\.)+[a-zA-Z]{2,7})', file)
+	def __init__(self, fn="data.bin"):
+		print('Iniciando com o banco %s' % fn)
 		
 		# Loop por domínios
-		for dm in r:
-			# Try
+		for dmn in self.db.getDomains(States.NEW):
+			# Nome do Domínio
+			dm = dmn['domain']
+			wr = lambda:None
 			try:
 				# Requisição Usando isAvail (CGI)
 				resp = requests.get(url="https://registro.br/cgi-bin/avail/?qr=%s" % dm, timeout=10)
-				wr = lambda:None
 				wr.__dict__ = json.loads(resp.text)
-
+				
 				# Checa se é válido
 				if(wr.available):
 					print("%s Domínio %s disponível." % (colored('✓', 'green', attrs=['bold']), colored(dm, 'green')))
-					self.avail.add(dm)
+					self.db.setState(dm, States.AVAIL)
 				elif('aguardando' in wr.reason):
 					print("%s Domínio %s aguardando próximo processo de liberação." % (colored('◆', 'yellow', attrs=['bold']), colored(dm, 'yellow')))
-					self.delayed.add(dm)
+					self.db.setState(dm, States.WAITING)
 				elif('participado' in wr.reason):
 					print("%s Domínio %s bloqueado por ter participado de 6 processos." % (colored('⦰', 'grey', attrs=['dark']), colored(dm, 'grey')))
-					self.locked.add(dm)
+					self.db.setState(dm, States.LOCKED)
 				else:
 					print("%s Domínio %s já registrado. (%s)" % (colored('✗', 'red', attrs=['bold']), colored(dm,'red'),colored(wr.reason,'red')))
-					self.taken.add(dm)
+					self.db.setState(dm, States.TAKEN)
+			# Sai do loop em caso de ctrl+c
 			except KeyboardInterrupt:
 				break
+			# Continua caso erro comum (rede, timeout, etc)
 			except:
 				print("Unexpected error:", sys.exc_info()[0])
+				self.db.setError(dm, sys.exc_info()[0])
 				continue
 
-		open('res_avail.txt','w').write('\n'.join(str(e) for e in self.avail))
-		open('res_taken.txt','w').write('\n'.join(str(e) for e in self.taken))
-		open('res_delayed.txt','w').write('\n'.join(str(e) for e in self.delayed))
-		open('res_locked.txt','w').write('\n'.join(str(e) for e in self.locked))
-
+		# Salva outputs
+		self.db.save()
+		self.db.saveJSON()
 # Iniciando
-dc = DomainChecker(sys.argv[1])
+dc = DomainChecker()
